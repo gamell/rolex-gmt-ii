b624a26d1ecc8a4eac9b64dede56589a2e95ef77
Joan Gamell <jgamell@linkedin.com>
2017-12-14 18:09:45 -0800

    Compiling CSS into a file instead of inlining into js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,16 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const path = require('path');
+
+const extractSass = new ExtractTextPlugin({
+    filename: "[name].[contenthash].css"
+});
+
+const pathsToClean = [
+  'bin'
+]
 
 module.exports = {
   entry: './src/app.js',
@@ -12,8 +22,16 @@ module.exports = {
     loaders: [
       { test: /\.pug$/, loader: 'pug-loader' },
       { test: /\.png$/, loader: 'file-loader' },
-      { test: /\.scss$/, loaders: ['style-loader', 'css-loader', 'sass-loader']},
-    ]
+      { test: /\.css$/, loader: 'file-loader' },
+      { test: /\.scss$/, use: extractSass.extract({
+        use: [{
+          loader: 'css-loader'
+        },{
+          loader: 'sass-loader'
+        }],
+        fallback: 'style-loader'
+      })
+    }]
   },
   plugins: [
     new webpack.optimize.UglifyJsPlugin({
@@ -26,8 +44,11 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: 'src/templates/index.pug',
-      filename: 'rolex.html'
-    })
+      filename: 'rolex.html',
+      css: ["style.css"]
+    }),
+    new CleanWebpackPlugin(pathsToClean),
+    extractSass
   ],
   resolve: {
     alias: {
